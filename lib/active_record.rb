83dc40d97704fb09b91b138727ef192e399374b6
macournoyer <macournoyer@gmail.com>
2013-05-17 12:44:57 -0400

    Implement Relation query API.

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -23,10 +23,6 @@ module ActiveRecord
       find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
     end
 
-    def self.all
-      find_by_sql("SELECT * FROM #{table_name}")
-    end
-
     def self.find_by_sql(sql)
       rows = @@connection.execute(sql)
       # rows = [{:id=>1, :name=>"Marc"}]
@@ -42,5 +38,23 @@ module ActiveRecord
     def self.table_name
       name.downcase + "s"  # "users"
     end
+
+    ## Relation
+    class << self
+      attr_accessor :current_scope
+    end
+
+    def self.all
+      if current_scope
+        current_scope.clone
+      else
+        Relation.new(self)
+      end
+    end
+
+    def self.where(values)
+      all.where(values)
+    end
+
   end
 end
---
+++ test/relation_test.rb
@@ -0,0 +1,35 @@
+require File.dirname(__FILE__) + '/test_helper'
+require "active_record"
+require "relation"
+
+class RelationTest < Test::Unit::TestCase
+  def setup
+    @relation = ActiveRecord::Relation.new(User)
+  end
+
+  def test_where
+    relation = @relation.where("name = 'Marc'")
+    assert_equal "SELECT * FROM users WHERE name = 'Marc' AND id = 1", relation.where("id = 1").to_sql
+    assert_equal "SELECT * FROM users WHERE name = 'Marc'", relation.to_sql
+  end
+
+  def test_to_a
+    user = @relation.where("id = 1").to_a.first
+    assert_equal 1, user.id
+  end
+  
+  def test_proxy_method_calls_to_a
+    user = @relation.where("id = 1").first
+    assert_equal 1, user.id
+  end
+  
+  def test_proxy_method_calls_to_a
+    user = User.where("id = 1").first
+    assert_equal 1, user.id
+  end
+
+  def test_proxy_class_methods
+    sql = User.where("id = 1").search("Marc").to_sql
+    assert_equal "SELECT * FROM users WHERE id = 1 AND name LIKE '%Marc%'", sql
+  end
+end
